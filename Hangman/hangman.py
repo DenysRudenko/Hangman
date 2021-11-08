cc6411637dfb467596130fda6ac507a8c015fd77
Denys Rudenko <heartbreathing19951014@gmail.com>
2021-11-08 18:27:11 +0200

    Hangman:added main game file

---
+++ Hangman/hangman.py
@@ -0,0 +1,104 @@
+import random
+import string
+
+wordlist_path =  "D://Labs_python//words.txt"
+
+def load_words(wordlist_path):
+    print("Loading word list from file...")
+    inFile = open(wordlist_path, 'r')
+    line = inFile.readline()
+    wordlist = line.split()
+    print(" ", len(wordlist), "words loaded.")
+    return wordlist
+
+wordlist = load_words(wordlist_path)
+
+def select_word(wordlist):
+    return random.choice(wordlist)
+
+def get_guessed_word(secret_word, letters_guessed):
+    letter_list = []
+    for letter in secret_word:
+        if letter in letters_guessed:
+            letter_list.append(letter)
+        else:
+            letter_list.append("_ ")
+            
+    return "".join(letter_list)
+
+def is_word_guessed(secret_word, letters_guessed):
+    first_set = set(secret_word)
+    second_set = set(letters_guessed)
+    difference = first_set - second_set
+    return len(difference) == 0
+
+def get_available_letters(letters_guessed):
+    available_letters = []
+    abc = string.ascii_lowercase
+    for letter in abc:
+        if letter not in letters_guessed:
+            available_letters.append(letter)
+    return "".join(available_letters)
+
+def hangman(secret_word):
+    letters_guessed = []
+    guesses_remaining = 6
+    warnings_remaining = 3
+    vowels = "aeiou"
+
+
+    print("Welcome to the game Hangman!")
+    print(f'I am thinking of a word that is {len(secret_word)} letters long.')
+    print(f'You have {warnings_remaining} warnings left.')
+   
+    while (guesses_remaining > 0) and not is_word_guessed(secret_word, letters_guessed):
+        print("-" * 12)
+        print(f'You have {guesses_remaining} guesses left.')
+        print(f'Available letters: {get_available_letters(letters_guessed)}')
+
+        wrong_input = False
+        letter = input("Please guess a letter:")
+        if len(letter) == 0:
+            print("Oops! This is no input letter. ", end="")
+            wrong_input = True
+        
+        elif len(letter) > 1:
+            print(f'Opps! You have entered {len(letter)} letters. ', end="")
+            wrong_input = True
+        else:
+            if letter.lower() not in string.ascii_lowercase:
+                print(f'Oops! "{letter}" is not a valid letter. ', end="")
+                wrong_input = True
+            else:
+                letter = letter.lower()
+                if letter in letters_guessed:
+                    print("Opps! You`ve alredy guessed that letter. ", end="")
+                    wrong_input = True
+                
+        if wrong_input:
+            if warnings_remaining > 0:
+                warnings_remaining -= 1
+                print(f'You have {warnings_remaining} warnings left: ', end="")
+            else:
+                guesses_remaining -= 1
+          
+        else:
+            letters_guessed.append(letter)
+            if letter in secret_word:
+                print("Good gueess: ", end="")
+            else:
+                guesses_remaining -= 2 if letter in vowels else 1    
+                print("Oops! That letter is not in my word: ", end="")
+
+        print(get_guessed_word(secret_word, letters_guessed ))
+    
+    print("-" * 12)
+    
+    if guesses_remaining > 0:
+        total_score = guesses_remaining * len(set(secret_word))
+        print(f'Congratulations, you won! Your total score for this game is: {total_score}.')
+    else:
+        print(f'Sorry, you ran out of guesses. The word was {secret_word}.')
+
+secret_word = select_word(wordlist)
+hangman(secret_word)
